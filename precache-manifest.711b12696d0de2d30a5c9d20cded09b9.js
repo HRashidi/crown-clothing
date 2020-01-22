406c3570bc0f8f6e3ece9c2235e4beaa3405202b
xlord <h.rashidi128@gmail.com>
2020-01-22 19:38:59 +0330

    Updates

---
+++ precache-manifest.711b12696d0de2d30a5c9d20cded09b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd31d90fe0c0b403b605354eefb42ee3",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "0e768bb3e9d16a048771",
+    "url": "/crown-clothing/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2249bfc1cffcd79e7099",
+    "url": "/crown-clothing/static/js/2.9d73a81b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crown-clothing/static/js/2.9d73a81b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e768bb3e9d16a048771",
+    "url": "/crown-clothing/static/js/main.8b1c97d6.chunk.js"
+  },
+  {
+    "revision": "5db7a3f1e721304dd7b4",
+    "url": "/crown-clothing/static/js/runtime-main.01c4097b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crown-clothing/static/media/logo.5d5d9eef.svg"
+  }
+]);
